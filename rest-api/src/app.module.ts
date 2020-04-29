97989d1d9aa7ca65063868d72f0ca076f74a5ba5
Nguyen Dai <ngtrdai290197@gmail.com>
2020-04-29 23:57:42 +0700

    (feat): add libs | init user base

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`,
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('/v1/api')
+
+  const config = app.get(ConfigService)
+
+  await app.listen(config.get<number>('PORT'), () => {
+    console.log(`Server started at port: ${config.get('PORT')} 🚀🚀🚀`)
+  })
 }
-bootstrap();
+bootstrap()
