b9a9d97d6b1a9ea05ee5ce06d19d8e75e3e3ca25
Nguyen Dai <ngtrdai290197@gmail.com>
2020-04-29 23:42:10 +0700

    (chore): remove core folder

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
-import { AppComponent } from './app.component'
 import { HttpClientModule } from '@angular/common/http'
 import { TranslocoRootModule } from './transloco-root.module'
-import { CoreModule } from '@core/core/core.module'
+
+import { CoreModule } from '@core/core.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
